c0fc85cbcc5387e482355af17422e7278a4d95c9
olegnik <olegnik11@ukr.net>
2024-01-27 21:08:13 +0200

    Second commit

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -1,8 +1,16 @@
 package com.lviv;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code her=
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedy text:");
+        String text = scanner.nextLine();
+        StringBuilder str = new StringBuilder();
+        str.append(text);
+        str = str.reverse();
+        System.out.println(str);
     }
 }
