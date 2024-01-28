795a329f7e329c1d89023b0533bd7f5fec0e2091
olegnik <olegnik11@ukr.net>
2024-01-28 21:35:41 +0200

    Fourth commit

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedy text:");
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedi text: ");
         String text = scanner.nextLine();
-        char symbols[] = text.toCharArray();
-        for (int x = symbols.length - 1; x >= 0; x--) {
-            System.out.print(symbols[x]);
+        String arr []= text.split("");
+        for (int i = arr.length-1; i >=0; i--){
+            System.out.print(arr[i]);
 
         }
+
     }
 }
