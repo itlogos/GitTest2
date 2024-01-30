61295a87aa45805546699d39ac577f6aceb277ef
olegnik <olegnik11@ukr.net>
2024-01-30 23:29:24 +0200

    12

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -1,20 +1,26 @@
 package com.lviv;
 
 import java.lang.reflect.Array;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedi text: ");
-        String text = scanner.nextLine();
-
-        String arr[] = text.split("");
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.print(arr[i]);
+        String sentense = scanner.nextLine();
+        String words[] = sentense.split(" ");
+        int size = words.length;
+        HashMap<String, Integer> key = new HashMap<String, Integer>();
+        for (int i = 0; i < size; i++) {
+            if (key.containsKey(words[i])) {
+                int counter = key.get(words[i]);
+                key.put(words[i], counter + 1);
+            } else {
+                key.put(words[i], 1);
+            }
 
         }
-
+        System.out.println(key);
     }
 }
