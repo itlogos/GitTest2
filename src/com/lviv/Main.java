fe59b0fe545f29caacd14ebb854b03ba075cbdcd
olegnik <olegnik11@ukr.net>
2024-01-31 21:50:57 +0200

    develop 1

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -5,23 +5,23 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String sentense = scanner.nextLine();
-        String words[] = sentense.split(" ");
-        int size = words.length;
+        System.out.println("Vvedy text: ");
+        String text = scanner.nextLine();
+        String words[] = text.split(" ");
         HashMap<String, Integer> key = new HashMap<String, Integer>();
-        for (int i = 0; i < size; i++) {
-            if (key.containsKey(words[i])) {
+        for (int i = 0; i < words.length; i++){
+            if (key.containsKey(words[i])){
                 int counter = key.get(words[i]);
-                key.put(words[i], counter + 1);
-            } else {
+                key.put(words[i], counter+1);
+            }
+            else {
                 key.put(words[i], 1);
             }
-
         }
         System.out.println(key);
+        //ddfssfddf
     }
+
 }
