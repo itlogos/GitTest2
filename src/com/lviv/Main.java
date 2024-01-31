1be0779d16fa5c911d77b0cc92a306f9775d3cae
olegnik <olegnik11@ukr.net>
2024-01-31 14:25:54 +0200

    13

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -5,15 +5,5 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedi text: ");
-        String text = scanner.nextLine();
-        String arr []= text.split("");
-        for (int i = arr.length-1; i >=0; i--){
-            System.out.print(arr[i]);
-//master
-        }
 
-    }
 }
