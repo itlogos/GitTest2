833005c01588969bd34c070c507cc6c3fcc5e2b9
olegnik <olegnik11@ukr.net>
2024-02-16 13:18:36 +0200

    Return to begin

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -7,21 +7,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedy text: ");
-        String text = scanner.nextLine();
-        String words[] = text.split(" ");
-        HashMap<String, Integer> key = new HashMap<String, Integer>();
-        for (int i = 0; i < words.length; i++){
-            if (key.containsKey(words[i])){
-                int counter = key.get(words[i]);
-                key.put(words[i], counter+1);
-            }
-            else {
-                key.put(words[i], 1);
-            }
-        }
-        System.out.println(key);
-        //ddfssfddf
+        System.out.println("Vvedi text:");
+       String text = scanner.nextLine();
+       StringBuilder builder = new StringBuilder();
+       builder.append(text);
+       builder = builder.reverse();
+        System.out.println(builder);
     }
-
 }
