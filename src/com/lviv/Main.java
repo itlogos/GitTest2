cff3eeb5dd3c9b9f1e8ffffc0d28a959681638b1
olegnik <olegnik11@ukr.net>
2024-02-16 13:51:33 +0200

    Task2

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -1,9 +1,19 @@
 package com.lviv;
 
-import java.lang.reflect.Array;
-import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedy text: ");
+        String text = scanner.nextLine();
+        char symbols[] = text.toCharArray();
+        System.out.println(symbols);
+        for (int i = symbols.length - 1; i >= 0; i--) {
+            System.out.print(symbols[i]);
+        }
+
+    }
 }
