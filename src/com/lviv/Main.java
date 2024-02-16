cb570770395ddac114bb2f5e0572901c7ff4b866
olegnik <olegnik11@ukr.net>
2024-02-16 13:22:43 +0200

    Zero

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -5,13 +5,5 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedi text:");
-       String text = scanner.nextLine();
-       StringBuilder builder = new StringBuilder();
-       builder.append(text);
-       builder = builder.reverse();
-        System.out.println(builder);
-    }
+
 }
