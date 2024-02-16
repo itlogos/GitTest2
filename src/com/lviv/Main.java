04d9f0463d23df4b2b651105c9892d10e5504787
olegnik <olegnik11@ukr.net>
2024-02-16 13:25:15 +0200

    Zero main

--- src/com/lviv/Main.java
+++ src/com/lviv/Main.java
@@ -6,21 +6,5 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String sentense = scanner.nextLine();
-        String words[] = sentense.split(" ");
-        int size = words.length;
-        HashMap<String, Integer> key = new HashMap<String, Integer>();
-        for (int i = 0; i < size; i++) {
-            if (key.containsKey(words[i])) {
-                int counter = key.get(words[i]);
-                key.put(words[i], counter + 1);
-            } else {
-                key.put(words[i], 1);
-            }
 
-        }
-        System.out.println(key);
-    }
 }
